f2e1f594e820e419058043a83dc5ddada998f119
dmhisdmh <iamdmhiu@gmail.com>
2024-12-06 17:17:56 +0700

    Dashboard admin

--- src/main/java/ute/shop/controller/HomeController.java
+++ src/main/java/ute/shop/controller/HomeController.java
@@ -11,24 +11,26 @@ import jakarta.servlet.http.HttpSession;
 import ute.shop.entity.User;
 
 @SuppressWarnings("serial")
-@WebServlet(urlPatterns = { "/home" })
+@WebServlet(urlPatterns = { "/home"})
 public class HomeController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// Get the current session, but don't create a new one if it doesn't exist
-		HttpSession session = req.getSession(false);
-
-		User user = null;
-		if (session != null) {
-			user = (User) session.getAttribute("account");
-		}
-
-		if (user != null) {
-			// If the user is logged in, display a personalized homepage
-			req.setAttribute("user", user);
-		}
-		// Forward to the homepage view (accessible by both logged-in and non-logged-in
-		// users)
-		req.getRequestDispatcher("/views/home.jsp").forward(req, resp);
+		
+		resp.sendRedirect(req.getContextPath() + "/admin/home");
+//		// Get the current session, but don't create a new one if it doesn't exist
+//		HttpSession session = req.getSession(false);
+//
+//		User user = null;
+//		if (session != null) {
+//			user = (User) session.getAttribute("account");
+//		}
+//
+//		if (user != null) {
+//			// If the user is logged in, display a personalized homepage
+//			req.setAttribute("user", user);
+//		}
+//		// Forward to the homepage view (accessible by both logged-in and non-logged-in
+//		// users)
+//		req.getRequestDispatcher("/views/home.jsp").forward(req, resp);
 	}
 }
